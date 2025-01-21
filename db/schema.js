91a7b6a74d8aa91353ec88141078454c31980620
prakhar17072004 <pmankur2004@gmail.com>
2025-01-21 17:28:55 +0530

    table is updated

--- db/schema.js
+++ db/schema.js
@@ -3,7 +3,7 @@ import { pgTable, serial, text, varchar, integer } from 'drizzle-orm/pg-core';
 // Define the "authers" table
 export const authers = pgTable('auther', {
     id: serial('id').primaryKey(), // Primary key for authors
-    auther_name: varchar('name', { length: 25 }).notNull(), // Name of the author
+    auther_name: varchar('auther_name', { length: 25 }).notNull(), // Name of the author
     email: varchar('email', { length: 25 }).notNull(), // Email of the author
     bio: text('bio'), // Bio of the author
 });
@@ -14,5 +14,5 @@ export const books = pgTable('books', {
     auther_id: integer('auther_id') // Foreign key referencing `authers.id`
         .notNull()
         .references(() => authers.id),
-    books_name: varchar('name', { length: 25 }).notNull(), // Name of the book
+    books_name: varchar('books_name', { length: 25 }).notNull(), // Name of the book
 });
